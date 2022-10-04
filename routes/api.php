19eb0165fcdc3af0da359c935a7c857fa8364482
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-10-04 17:43:15 +0200

    Posts da db a pagina

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/index', function(){
-    $names  = ['Emanuele', 'Luigi'];
-    return response()->json($names);
+Route::get('/', function(){
+    $posts = Post::all();
+    return response()->json( compact('posts') );
 });
