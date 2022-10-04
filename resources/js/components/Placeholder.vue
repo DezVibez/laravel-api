266449c31dbce687a34bc3e8c6e2c068d78bed06
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-10-04 19:57:13 +0200

    ora il run dev non da errori

---
+++ resources/js/components/Placeholder.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="title m-b-md">{{ title }}</div>
+</template>
+
+<script>
+    export default {
+        name: "Placeholder",
+        props: {
+            title: String,
+        },
+    };
+</script>
